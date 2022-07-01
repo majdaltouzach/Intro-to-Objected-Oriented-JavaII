6bcb698bc51fc4670293e24363219070776fd0e6
majdaltouzach <93485057+majdaltouzach@users.noreply.github.com>
2022-07-01 11:54:16 -0700

    New methods in Insect.java and created new InsectClient.java

--- Insect/src/InsectInstance/Insect.java
+++ Insect/src/InsectInstance/Insect.java
@@ -8,6 +8,12 @@ public class Insect {
 	//static constants/variables
 	public static final double DIST_WEIGHT_LOSS_FACTOR = .0001;
 	private static int population = 0;
+	private static final String[] FACTS = {
+			"The two main groups of insects are winged and wingless",
+			"There are more than 1 million insect species",
+			"Insects are cold-blooded",
+			"Spiders are not considered insects"
+	};
 	
 	public Insect(double initWeight, int initX, int initY) {
 		weight = initWeight;
@@ -38,16 +44,25 @@ public class Insect {
 	private static double calculateDistance(double x1, double y1, double x2, double y2) {
 		return Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
 	}
+	
+	public static String produceRandomFact() {
+		int index = (int)(Math.random() * ((FACTS.length - 1) + 1));
+		return FACTS[index];
+	}
 	//test method
 	public static void main(String[] args) {
 //		Insect buzz1 = new Insect();
 //		Insect buzz2 = new Insect();
+		System.out.println("Insect Population: " + population);
 		Insect bug1 = new Insect(10, 100, 90);
-		Insect bug2 = new Insect(4, -300, 400);
+		System.out.println("Insect Population: " + population);
+		Insect bug2 = new Insect(9.5, -300, 400);
+		System.out.println("Insect Population: " + population);
 		
 		bug1.move(1, 10);
 		bug2.move(-300, 400);
 		
+		System.out.println(produceRandomFact());
 	}
 }
 
