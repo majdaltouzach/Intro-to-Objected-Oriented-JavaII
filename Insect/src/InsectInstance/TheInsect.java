e2e59211e977366d100ea8b273b493c8afee8395
majdaltouzach <93485057+majdaltouzach@users.noreply.github.com>
2022-07-01 11:06:23 -0700

    New Code

--- Insect/src/InsectInstance/TheInsect.java
+++ Insect/src/InsectInstance/TheInsect.java
@@ -6,6 +6,11 @@ public class TheInsect {
 	private int y;
 	
 	
+	//test method
+	public static void main(String[] args) {
+		TheInsect buzz1 = new TheInsect();
+		TheInsect buzz2 = new TheInsect();
+	}
 }
 
 
