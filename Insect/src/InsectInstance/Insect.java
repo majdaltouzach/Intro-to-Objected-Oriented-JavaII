090ca8278f33a8eb0eb8aacfaf29b228d88d8926
majdaltouzach <93485057+majdaltouzach@users.noreply.github.com>
2022-07-01 11:17:52 -0700

    Created Insect Constructor

---
+++ Insect/src/InsectInstance/Insect.java
@@ -0,0 +1,22 @@
+package InsectInstance;
+
+public class Insect {
+	private double weight;
+	private int x;
+	private int y;
+	
+	public Insect(double initWeight, int initX, int initY) {
+		weight = initWeight;
+		x = initX;
+		y = initY;
+	}
+	//test method
+	public static void main(String[] args) {
+//		Insect buzz1 = new Insect();
+//		Insect buzz2 = new Insect();
+		Insect bug1 = new Insect(10, 100, 90);
+		Insect bug2 = new Insect(4, -300, 400);
+	}
+}
+
+
