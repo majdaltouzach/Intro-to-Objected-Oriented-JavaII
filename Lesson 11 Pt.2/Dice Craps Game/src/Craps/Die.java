95a3089cd9245f0ee93f3cfd195d92cb4976b42e
majdaltouzach <93485057+majdaltouzach@users.noreply.github.com>
2022-07-18 10:04:07 -0700

    Added toString, roll, and getFaceValue methods

--- Lesson 11 Pt.2/Dice Craps Game/src/Craps/Die.java
+++ Lesson 11 Pt.2/Dice Craps Game/src/Craps/Die.java
@@ -1,5 +1,33 @@
 package Craps;
 
-public class Die {
+import java.util.Random;
 
+public class Die {
+    public static final int SIDES = 6;
+    private int faceValue;
+    private Random rand;
+
+    public Die(){
+        faceValue = 1;
+        rand = new Random();
+    }
+
+    public int roll(){
+        faceValue = rand.nextInt(SIDES) + 1;
+        return  faceValue;
+    }
+
+    public int getFaceValue(){
+        return faceValue;
+    }
+
+    public String toString(){
+        return "Die with face values " + faceValue;
+    }
+    public static void main(String[] args) {
+        Die die1 = new Die();
+        System.out.println(die1.toString());
+        System.out.println(die1.roll());
+        System.out.println(die1.roll());
+    }
 }
