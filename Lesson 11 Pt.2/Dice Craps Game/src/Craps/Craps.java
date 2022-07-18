0f5e4d4c3abeed17c8b8b55b1a355a29d2fc2e8a
majdaltouzach <93485057+majdaltouzach@users.noreply.github.com>
2022-07-18 11:02:15 -0700

    Wrote Craps Game

---
+++ Lesson 11 Pt.2/Dice Craps Game/src/Craps/Craps.java
@@ -0,0 +1,58 @@
+package Craps;
+
+public class Craps {
+    private Die die1, die2;
+    private int point;
+
+    public Craps(){
+        die1 = new Die();
+        die2 = new Die();
+    }
+
+    private int toss(){
+        int total = die1.roll() + die2.roll();
+        System.out.println("Die One: " + die1.getFaceValue()
+        + ", Die Two: " + die2.getFaceValue());
+
+        return total;
+    }
+
+    private void go(){
+        point = toss();
+        System.out.println("Point: " + point);
+
+        if(point == 7 || point == 11){
+            System.out.println("Winner");
+        }
+        else if(point == 2 || point == 3 || point == 12){
+            System.out.println("You lose!");
+        }
+        else{
+            System.out.println("\nEntering Stage 2:");
+            stage2();
+        }
+    }
+
+    public void stage2(){
+        boolean endGame = false;
+
+        while(!endGame){
+            int total = toss();
+
+            System.out.println("Total " + total);
+            if(total == point){
+                System.out.println("Winner");
+            }
+            else if(total == 7){
+                System.out.println("You lost");
+                endGame = true;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Craps game = new Craps();
+
+        game.go();
+    }
+}
--- Lesson 11 Pt.2/Dice Craps Game/src/Craps/Die.java
+++ Lesson 11 Pt.2/Dice Craps Game/src/Craps/Die.java
@@ -3,7 +3,7 @@ package Craps;
 import java.util.Random;
 
 public class Die {
-    public static final int SIDES = 6;
+    private static final int SIDES = 6;
     private int faceValue;
     private Random rand;
 
@@ -14,7 +14,12 @@ public class Die {
 
     public int roll(){
         faceValue = rand.nextInt(SIDES) + 1;
-        return  faceValue;
+        return faceValue;
+
+        /*
+        We could also use the Math.random method
+        low (int) (Math.random() + ((high - low) + 1))
+         */
     }
 
     public int getFaceValue(){
@@ -22,12 +27,13 @@ public class Die {
     }
 
     public String toString(){
-        return "Die with face values " + faceValue;
+        return "Die with face value: " + faceValue;
     }
-    public static void main(String[] args) {
-        Die die1 = new Die();
-        System.out.println(die1.toString());
-        System.out.println(die1.roll());
-        System.out.println(die1.roll());
-    }
+//    public static void main(String[] args) {
+//        Die die1 = new Die();
+//
+//        System.out.println(die1.toString());
+//        System.out.println(die1.roll());
+//        System.out.println(die1.roll());
+//    }
 }
